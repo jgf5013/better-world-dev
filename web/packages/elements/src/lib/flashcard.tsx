41513c4dae29dd6fcc82d54b7fc2b91267bf091b
John Fisher <jgf5013@gmail.com>
2024-04-20 10:37:56 +0200

    fix: country change

--- web/packages/elements/src/lib/flashcard.tsx
+++ web/packages/elements/src/lib/flashcard.tsx
@@ -6,7 +6,7 @@ export type FlashcardProps = {}
 const Flashcard = (props: FlashcardProps) => {
   return (
     <div className={styles['container']}>
-      <h1>The United States</h1>
+      <h1>Dominican Republic</h1>
     </div>
   );
 };
