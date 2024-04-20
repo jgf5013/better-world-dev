3052940324b5d560902923874483da8434a51e5f
John Fisher <jgf5013@gmail.com>
2024-04-20 10:24:43 +0200

    moving some things around

--- web/packages/elements/src/lib/flashcard.tsx
+++ web/packages/elements/src/lib/flashcard.tsx
@@ -6,7 +6,7 @@ export type FlashcardProps = {}
 const Flashcard = (props: FlashcardProps) => {
   return (
     <div className={styles['container']}>
-      <h1>The Netherlands</h1>
+      <h1>The United States</h1>
     </div>
   );
 };
