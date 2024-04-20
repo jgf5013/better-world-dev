5013c92eabcb21dd2bc8f055605e3b6b21aafd47
John Fisher <jgf5013@gmail.com>
2024-04-20 19:22:48 +0200

    feat: adding another library

---
+++ web/packages/game/src/lib/game.tsx
@@ -0,0 +1,14 @@
+import styles from './game.module.css';
+
+/* eslint-disable-next-line */
+export interface GameProps {}
+
+export function Game(props: GameProps) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Game!</h1>
+    </div>
+  );
+}
+
+export default Game;
