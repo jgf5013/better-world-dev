47fdb15114b1cec532654dcb0e9fcb2603ff3ab6
John Fisher <jgf5013@gmail.com>
2024-04-21 22:28:40 +0200

    fix: country 3

--- web/packages/elements/src/lib/flashcard.tsx
+++ web/packages/elements/src/lib/flashcard.tsx
@@ -6,7 +6,7 @@ export type FlashcardProps = {}
 const Flashcard = (props: FlashcardProps) => {
   return (
     <div className={styles['container']}>
-      <h1>Country 2</h1>
+      <h1>Country 3</h1>
     </div>
   );
 };
