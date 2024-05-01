7b17fc34e21c3f8327a68f439f9dd3c6acdd85bc
John Fisher <jgf5013@gmail.com>
2024-05-01 18:56:37 +0200

    fix: update

--- web/packages/elements/src/lib/Flashcard.tsx
+++ web/packages/elements/src/lib/Flashcard.tsx
@@ -26,7 +26,7 @@ export const Flashcard = ({card, onClick}: FlashcardPropsType) => {
   const Text = (
     <div style={{ backgroundColor: "#859398", borderRadius: "0.15rem" }}>
       <RadixText as="p" size="3">
-        <Strong>{card.sideShown === 'question' ? 'Name A' : card.answer}</Strong>
+        <Strong>{card.sideShown === 'question' ? 'Name' : card.answer}</Strong>
       </RadixText>
     </div>
   );
